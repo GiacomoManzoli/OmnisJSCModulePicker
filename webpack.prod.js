676da09129acb779142344e759ef8f7de11252fe
Giacomo Manzoli <giacomo.manzoli@gmail.com>
2025-05-27 18:26:04 +0200

    Version banner on prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,12 +2,16 @@ const { merge } = require("webpack-merge")
 const dev = require("./webpack.dev.js")
 const TerserPlugin = require("terser-webpack-plugin")
 
+const webpack = require("webpack")
+const packageJson = require("./package.json")
+
 module.exports = merge(dev, {
     mode: "production",
     optimization: {
         minimize: true,
         minimizer: [
             new TerserPlugin({
+                extractComments: false,
                 terserOptions: {
                     compress: {
                         pure_funcs: ["console.debug"], // oppure drop_console: true
@@ -16,4 +20,11 @@ module.exports = merge(dev, {
             }),
         ],
     },
+    plugins: [
+        new webpack.BannerPlugin({
+            banner: `Version: ${packageJson.version} - Build date: ${new Date().toISOString()}`,
+            raw: false,
+            entryOnly: true,
+        }),
+    ],
 })
